08f0236e24e831c48a1fa0067d292e897f6117f5
Nathan Marasigan <egarsenal16@gmail.com>
2016-07-06 01:36:08 -0500

    added user servce

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'angular2-cookie/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { UrlService } from './url.service';
+
+@Injectable()
+export class UserService{
+	constructor(private url: UrlService, private cookie: CookieService, private http: Http){
+	}
+	private status: Observable<string>;
+	private officer: Observable<boolean>;
+
+	getUser(){
+		let url = this.url.getAPIUrl() + '/user/status/';
+		let headers = new Headers();
+		this.url.appendHeaders(headers);
+		return this.http.get(url, {headers: headers})
+		           .map(this.extractData)
+	               .catch(this.handleError);
+	}
+	getOfficer(){
+		let url = this.url.getAPIUrl() + '/officer/';
+		let headers = new Headers();
+		this.url.appendHeaders(headers);
+		return this.http.get(url, {headers: headers})
+	               .catch(this.handleError);
+	}
+
+	//logout is placed in here so that more services don't need to be injected into the main appcomponent
+	logout(){
+		this.cookie.remove('Token');
+	}
+	private handleError (error: any) {
+	  // In a real world app, we might use a remote logging infrastructure
+	  // We'd also dig deeper into the error to get a better message
+	  let errMsg = (error.message) ? error.message :
+	    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+	  console.error(errMsg); // log to console instead
+	  return Observable.throw(errMsg);
+	}
+	private extractData(res: Response){
+		let body = res.json()
+		return body || {};
+	}
+}
